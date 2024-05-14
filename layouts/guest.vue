ffc588fd9926d97d5ada28a94bfbeed57ce19c56
vdyotte <vdyotte@gmail.com>
2024-05-14 12:25:50 -0400

    make dummy header taller

--- layouts/guest.vue
+++ layouts/guest.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="main bg-base-200 w-full min-h-screen">
         <div class="w-full flex justify-center bg-base-100 sticky top-0 z-20 drop-shadow-lg py-1 px-5">
-            <div class="xl:w-3/4 max-w-screen-xl" />
+            <div class="xl:w-3/4 max-w-screen-xl h-12" />
         </div>
         <slot class="h-screen flex justify-center page" />
         <div class="w-full px-5" />
